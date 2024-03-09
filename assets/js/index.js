7085fa23c06a66ef93ae14096e413ba219317056
manuel4life <allotey4eva@gmail.com>
2024-03-09 15:31:57 +0100

    added fetch movies,display movies,next and previous features + replaced API with local JSON data sets

---
+++ assets/js/index.js
@@ -0,0 +1,57 @@
+window.addEventListener('load', function() {
+    const moviesSection = document.querySelector('#movies-container');
+    const nextButton = document.querySelector('#next');
+    const previousButton = document.querySelector('#prev');
+
+
+
+    const endPoint = "assets/mockData/data.json"; //https://moviesdatabase.p.rapidapi.com/titles/x/upcoming?page=1
+    const headers = {
+        "X-RapidAPI-Key": "cea8f882f0msh261f8eec46fd5c0p11a39cjsn5eb4ce897a92",
+        "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com"
+    };
+    displayMovies();
+
+    async function getMovies(){
+        const response = await fetch(endPoint, {
+            headers: headers
+        });
+        const data = await response.json();
+        return data;
+    }
+
+        
+    async function displayMovies(){
+        const movies = await getMovies();
+        for(let i = 0; i < movies.length; i++){
+            const movieCard = `  
+            <div class="movie">
+            <h2>${movies[i].title}</h2>
+            </div>`;
+
+            moviesSection.innerHTML += movieCard;
+        }
+
+        const movieCards = document.querySelectorAll('.movie');
+        movieCards.forEach(function(movieCard){
+            movieCard.addEventListener('click', function(){
+                //display movie details
+                console.log(this.children[0].textContent);
+            });
+        });
+
+    }
+
+    nextButton.addEventListener('click', function(){
+        //load more movies
+
+    });
+
+
+    previousButton.addEventListener('click', function(){
+        // load previous movies sets
+
+
+    });
+
+});
