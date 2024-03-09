ab9746668f59d885dfd4645b1d05c008c984172f
manuel4life <allotey4eva@gmail.com>
2024-03-09 15:49:50 +0100

    error handling

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,10 +2,11 @@ window.addEventListener('load', function() {
     const moviesSection = document.querySelector('#movies-container');
     const nextButton = document.querySelector('#next');
     const previousButton = document.querySelector('#prev');
+    const errorBadge = document.querySelector('#error-badge');
 
 
 
-    const endPoint = "assets/mockData/data.json"; //https://moviesdatabase.p.rapidapi.com/titles/x/upcoming?page=1
+    const endPoint = "assets/mockData/data.json"; //'https://moviesdatabase.p.rapidapi.com/titles/x/upcoming?page=1';
     const headers = {
         "X-RapidAPI-Key": "cea8f882f0msh261f8eec46fd5c0p11a39cjsn5eb4ce897a92",
         "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com"
@@ -16,6 +17,12 @@ window.addEventListener('load', function() {
         const response = await fetch(endPoint, {
             headers: headers
         });
+
+        if(!response.ok){
+            errorBadge.style.display = 'block';
+            throw new Error('Network response was not ok');
+            return;
+        }
         const data = await response.json();
         return data;
     }
